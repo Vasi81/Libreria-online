b144b927409acc28e4b60a263117588c916f3552
Vesselin Stanev <78701093+Vasi81@users.noreply.github.com>
2022-11-30 22:48:17 +0100

    Creación de querys en LibroRepository
    Creación de querys para los métodos de findByPalabra() y findByTema().

--- src/main/java/com/libreria/repository/LibroRepository.java
+++ src/main/java/com/libreria/repository/LibroRepository.java
@@ -1,9 +1,19 @@
 package com.libreria.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.libreria.entitybeans.Libro;
 
 public interface LibroRepository extends JpaRepository<Libro, Long> {
+	
+	@Query ("select l from Libro l where l.titulo like %:titulo%")
+	public List<Libro>findByPalabra(String titulo);
+	
+	@Query("select l from Libro l where l.tema.descTema=?1")
+	public List<Libro>findByTema(String descTema);
+	
 
 }
